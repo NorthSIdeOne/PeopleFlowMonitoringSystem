1758f58a38aaa27d75189a13f4ac0415c96a68f7
NorthSIdeOne <beatmix45@gmail.com>
2021-02-26 15:39:37 +0200

    Added a config file +comments on dbconfig

---
+++ backend/config/configurationFile.js
@@ -0,0 +1,14 @@
+/**
+ * This file contains all constants used in the project
+ * and it is used as configuration file.
+ */
+
+module.exports = {
+
+// Database
+HOST     : "localhost",
+USER     :  "root",
+PASSWORD :  "",
+DATABASE :  "sniffer_test",
+
+}
--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,19 +1,33 @@
-'use strict';
+/**
+ * This file is used to connect to the mysql database.
+ * @dbCon the mysql object which is used to create a connection
+ *        and to connect to the databse.
+ */
 
+'use strict';
+const config = require('./configurationFile');
 const mysql = require('mysql');
 
 const dbCon = mysql.createConnection({
 
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "sniffer_test"
+    host:     config.HOST,
+    user:     config.USER,
+    password: config.PASSWORD,
+    database: config.DATABSE
 
 });
 
+/**
+ *  Connect to the database.If the databse is not runnig
+ * an error will be displayed.If is working a message will
+ * be displayed.
+ */
 dbCon.connect(function(err) {
-    if (err) throw err;
-    console.log("Database Connected!");
+    if (err) {
+      console.error('Error connecting: ' + err.stack);
+      return;
+    }
+    console.log('Connected as id ' + dbCon.threadId);
   });
 
   module.exports=dbCon;
