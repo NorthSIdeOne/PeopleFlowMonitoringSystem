9acecae2926fa65a0aba8d29a8920375f5f6e843
NorthSIdeOne <beatmix45@gmail.com>
2021-03-29 22:11:28 +0300

    PFMS0004: -Added comments

--- backend/routes/api/PeopleFlow.js
+++ backend/routes/api/PeopleFlow.js
@@ -4,11 +4,26 @@ const router = express.Router();
 const proccessDataView =   require('../../views/ProcessData.view');
 
 
+/**
+ *  Get an array with all the rooms from the
+ *  database
+ */
 
 router.get('/rooms',async (req,res)=>{
 
     res.send(await proccessDataView.getRooms());
     console.log("Get rooms")
+});
+
+/**
+ *  Get all the data with the number of people from the
+ *  database
+ */
+
+router.get('/nr_of_people',async (req,res)=>{
+
+    res.send(await proccessDataView.getNrOfPeople());
+    console.log("Get nr_of_people")
 });
 
 
