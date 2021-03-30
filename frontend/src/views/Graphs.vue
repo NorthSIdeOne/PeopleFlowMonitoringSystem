8d0df99b4e92046e0991350f4c570398640397d1
NorthSIdeOne <beatmix45@gmail.com>
2021-03-30 09:42:02 +0300

    PFMS0004: -Added Graphs component

---
+++ frontend/src/views/Graphs.vue
@@ -0,0 +1,100 @@
+<template>
+
+  <v-container>
+    <v-row>
+<v-col>
+      <v-sheet color="rgba(0, 0, 0, .12)">
+        <v-sparkline
+            :value="value"
+            color="rgba(255, 255, 255, .7)"
+            height="100"
+            padding="24"
+            stroke-linecap="round"
+            smooth
+        >
+          <template v-slot:label="item">
+            ${{ item.value }}
+          </template>
+        </v-sparkline>
+      </v-sheet>
+   </v-col>
+      <v-col>
+
+
+        <v-sheet color="rgba(0, 0, 0, .12)">
+          <v-sparkline
+              :value="value"
+              color="rgba(255, 255, 255, .7)"
+              height="100"
+              padding="24"
+              stroke-linecap="round"
+              smooth
+          >
+            <template v-slot:label="item">
+              ${{ item.value }}
+            </template>
+          </v-sparkline>
+        </v-sheet>
+      </v-col>
+
+
+
+    </v-row>
+    <v-row>
+      <v-col>
+        <v-sheet color="rgba(0, 0, 0, .12)">
+          <v-sparkline
+              :value="value"
+              color="rgba(255, 255, 255, .7)"
+              height="100"
+              padding="24"
+              stroke-linecap="round"
+              smooth
+          >
+            <template v-slot:label="item">
+              ${{ item.value }}
+            </template>
+          </v-sparkline>
+        </v-sheet>
+      </v-col>
+      <v-col>
+
+
+        <v-sheet color="rgba(0, 0, 0, .12)">
+          <v-sparkline
+              :value="value"
+              color="rgba(255, 255, 255, .7)"
+              height="100"
+              padding="24"
+              stroke-linecap="round"
+              smooth
+          >
+            <template v-slot:label="item">
+              ${{ item.value }}
+            </template>
+          </v-sparkline>
+        </v-sheet>
+      </v-col>
+
+
+
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    value: [
+      423,
+      446,
+      675,
+      510,
+      590,
+      610,
+      760,
+    ],
+  }),
+}
+</script>
+
